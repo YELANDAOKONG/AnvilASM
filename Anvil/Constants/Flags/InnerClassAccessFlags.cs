f9f32677964be506940e9057eb79e0c63aa515bb
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-12-24 19:22:15 +0800

    Add InnerClassAccessFlags enum for JVM inner class access modifiers

---
+++ Anvil/Constants/Flags/InnerClassAccessFlags.cs
@@ -0,0 +1,16 @@
+namespace Anvil.Constants.Flags;
+
+[Flags]
+public enum InnerClassAccessFlags : ushort
+{
+    Public = 0x0001,
+    Private = 0x0002,
+    Protected = 0x0004,
+    Static = 0x0008,
+    Final = 0x0010,
+    Interface = 0x0200,
+    Abstract = 0x0400,
+    Synthetic = 0x1000,
+    Annotation = 0x2000,
+    Enum = 0x4000
+}
